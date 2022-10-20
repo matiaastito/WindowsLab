c7deb04ebf593557c6cd6a3b508c4f344a470232
Matias <matias.tito27@gmail.com>
2022-10-20 17:04:26 -0300

    Agrego caso 6

---
+++ WindowsLab/FrmCaso6.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsLab
+{
+    partial class FrmCaso6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsertDatos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInsertDatos
+            // 
+            this.btnInsertDatos.Location = new System.Drawing.Point(141, 154);
+            this.btnInsertDatos.Name = "btnInsertDatos";
+            this.btnInsertDatos.Size = new System.Drawing.Size(167, 63);
+            this.btnInsertDatos.TabIndex = 0;
+            this.btnInsertDatos.Text = "Insertar Datos";
+            this.btnInsertDatos.UseVisualStyleBackColor = true;
+            this.btnInsertDatos.Click += new System.EventHandler(this.btnInsertDatos_Click);
+            // 
+            // FrmCaso6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnInsertDatos);
+            this.Name = "FrmCaso6";
+            this.Text = "FrmCaso6";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsertDatos;
+    }
+}
---
+++ WindowsLab/FrmCaso6.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsLab
+{
+    public partial class FrmCaso6 : Form
+    {
+        public FrmCaso6()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertDatos_Click(object sender, EventArgs e)
+        {
+            string nombreCompleto = Interaction.InputBox("Ingrese su nombre completo", "Carga datos");
+            DateTime fechaNacimiento = DateTime.Parse(Interaction.InputBox("Ingrese su fecha de nacimiento con formato dd/mm/yyyy"));
+            imprimir(nombreCompleto);
+            imprimir(nombreCompleto, fechaNacimiento);
+        }
+
+        private void imprimir (string nombreCompleto)
+        {
+            MessageBox.Show(nombreCompleto);               
+        }
+
+        private void imprimir (string nombreCompleto, DateTime fechaNacimiento)
+        {
+            int edad = calcularEdad(fechaNacimiento);
+            MessageBox.Show("Nombre: " + nombreCompleto + "\nEdad: " + edad);
+        }
+
+        private int calcularEdad(DateTime fechaNacimiento) 
+        {
+            int edad = 0;
+            DateTime fechaActual = DateTime.Today;
+            edad = fechaActual.Year - fechaNacimiento.Year;
+            return edad;
+        }
+    }
+}
