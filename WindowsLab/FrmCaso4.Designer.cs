8b7d00a9906a338e9a320e5c44cc0494ff2601bf
Matias <matias.tito27@gmail.com>
2022-10-20 14:50:46 -0300

    Agrego caso 4

---
+++ WindowsLab/FrmCaso4.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsLab
+{
+    partial class FrmCaso4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcularArea = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCalcularArea
+            // 
+            this.btnCalcularArea.Location = new System.Drawing.Point(354, 167);
+            this.btnCalcularArea.Name = "btnCalcularArea";
+            this.btnCalcularArea.Size = new System.Drawing.Size(134, 42);
+            this.btnCalcularArea.TabIndex = 0;
+            this.btnCalcularArea.Text = "Calcular Area";
+            this.btnCalcularArea.UseVisualStyleBackColor = true;
+            this.btnCalcularArea.Click += new System.EventHandler(this.btnCalcularArea_Click);
+            // 
+            // FrmCaso4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcularArea);
+            this.Name = "FrmCaso4";
+            this.Text = "FrmCaso4";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcularArea;
+    }
+}
---
+++ WindowsLab/FrmCaso4.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsLab
+{
+    public partial class FrmCaso4 : Form
+    {
+        const double PI = 3.14;
+        public FrmCaso4()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcularArea_Click(object sender, EventArgs e)
+        {
+            decimal resultado = 0;
+            
+            MessageBox.Show("A continuacion se le pedira que ingrese un numero." +
+                "\nSi desea calcular el area de un circulo, presione 1." +
+                "\nSi desea calcular el area de un trapecio, presione 2." +
+                "\nSi desea calcular el area de un rectangulo, presione 3" +
+                "\nIngrese 0 para finalizar.");
+            int opcArea = int.Parse(Interaction.InputBox("Ingrese un numero:"));
+            switch (opcArea)
+            {
+                case 1:
+                    resultado = (decimal)areaCirculo();
+                    MessageBox.Show("El area del circulo es " + resultado);
+                    break;
+                case 2:
+                    resultado = areaTrapecio();
+                    MessageBox.Show("El area del trapecio es " + resultado);
+                    break;
+                case 3:
+                    resultado = areaRectangulo();
+                    MessageBox.Show("El area del rectangulo es " + resultado);
+                    break;
+                case 0:
+                    break;
+            }
+        }
+
+        private decimal areaRectangulo()
+        {
+            decimal bRectangulo = decimal.Parse(Interaction.InputBox("Ingrese la base del Rectangulo:", "Carga datos"));
+            decimal hRectangulo = decimal.Parse(Interaction.InputBox("Ingrese la altura del Rectangulo:", "Carga datos"));
+            return bRectangulo * hRectangulo;
+        }
+
+        private decimal areaTrapecio()
+        {
+            decimal bMenorTrapecio = decimal.Parse(Interaction.InputBox("Ingrese la base menor del Trapecio:", "Carga datos"));
+            decimal bMayorTrapecio = decimal.Parse(Interaction.InputBox("Ingrese la base mayor del Trapecio:", "Carga datos"));
+            decimal hTrapecio = decimal.Parse(Interaction.InputBox("Ingrese la altura del Trapecio:", "Carga datos"));
+            return (bMayorTrapecio+bMenorTrapecio)*hTrapecio/2;
+        }
+
+        private double areaCirculo()
+        {
+            double rCirculo = double.Parse(Interaction.InputBox("Ingrese el Radio del Circulo:", "Carga datos"));
+            return PI * rCirculo * rCirculo;
+        }
+    }
+}
