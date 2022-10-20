4140e9830e50d067b5b52154272c33c3a625e8e4
Matias <matias.tito27@gmail.com>
2022-10-20 15:23:13 -0300

    Agrego caso 5

---
+++ WindowsLab/FrmCaso5.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsLab
+{
+    partial class FrmCaso5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewUser = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNewUser
+            // 
+            this.btnNewUser.Location = new System.Drawing.Point(281, 159);
+            this.btnNewUser.Name = "btnNewUser";
+            this.btnNewUser.Size = new System.Drawing.Size(168, 68);
+            this.btnNewUser.TabIndex = 0;
+            this.btnNewUser.Text = "Nuevo Usuario";
+            this.btnNewUser.UseVisualStyleBackColor = true;
+            this.btnNewUser.Click += new System.EventHandler(this.btnNewUser_Click);
+            // 
+            // FrmCaso5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnNewUser);
+            this.Name = "FrmCaso5";
+            this.Text = "FrmCaso5";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewUser;
+    }
+}
---
+++ WindowsLab/FrmCaso5.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsLab
+{
+    public partial class FrmCaso5 : Form
+    {
+        public FrmCaso5()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNewUser_Click(object sender, EventArgs e)
+        {
+            string nombreUsuario = Interaction.InputBox("Ingrese el nombre");
+            char sexoUsuario = char.Parse(Interaction.InputBox("Ingrese el sexo(M para masculino, F para Femenino)"));
+            string tipoUsuario = Interaction.InputBox("Ingrese el tipo de usuario. Los tipos validos son Administrador," +
+                "Estándar o Invitado");
+            while (validarTipo(tipoUsuario) == 0)
+            {
+                tipoUsuario = Interaction.InputBox("Tipo invalido. Ingrese el tipo de usuario. " +
+                    "Los tipos validos son Administrador,Estándar o Invitado");
+            }
+            while (validarSexo(Convert.ToString(sexoUsuario)) == 0)
+            {
+                sexoUsuario = char.Parse(Interaction.InputBox("Sexo invalido. Ingrese el sexo(M para masculino, F para Femenino"));
+            } 
+            MessageBox.Show("Usuario cargado con exito\n" + cargaUser(nombreUsuario, tipoUsuario, Convert.ToString(sexoUsuario)));
+
+
+        }
+
+        private int validarTipo(string tUsuario)
+        {
+            int flag = 0;
+            tUsuario = tUsuario.ToUpper();
+            if (string.Compare(tUsuario, "ADMINISTRADOR") ==0 || string.Compare(tUsuario, "ESTÁNDAR") == 0 || 
+                string.Compare(tUsuario, "INVITADO") == 0){
+                flag = 1;
+            }
+            return flag;
+        } 
+
+        private int validarSexo(string sUsuario)
+        {
+            int flag = 0;
+            sUsuario = sUsuario.ToUpper();
+            if (string.Compare(sUsuario, "F") == 0 || string.Compare(sUsuario, "M") == 0)
+            {
+                flag = 1;
+            }
+            
+            return flag;
+        }
+
+        private string cargaUser (string nUsuario, string tUsuario, string sUsuario)
+        {
+            string newUser = "";
+            newUser = "Usuario: " + nUsuario + "\nSexo: " + sUsuario + "\nRol: " + tUsuario;
+            return newUser;
+        }
+    }
+}
